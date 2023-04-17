29c08b1c6e6c41c292d425be56cffe5e7bc53475
evolpe <elliotvolpe123@hotmail.co.uk>
2023-04-17 07:57:04 +0100

    Day 33: Filtering forecast data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
+
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
@@ -11,13 +13,7 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10","2022-27-10"]
-    temperatures = [10, 11, 12]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-d, t = get_data(days)
+data = get_data(pace, days, option)
 
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "temperatures(C)"})
 st.plotly_chart(figure)
